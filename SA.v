434d0069a9553eaaf80f636e75c7a6013af2c24f
Akhilendra Nair <mossbeachbrothers@gmail.com>
2024-08-13 14:00:24 -0700

    structure for SA

--- SA.v
+++ SA.v
@@ -1,1 +1,13 @@
 //Systolic Array Module for M x N matrix
+module SystolicArray #(parameter M = 4, paramter K = 4, parameter N = 4, DATA_WIDTH=8)(
+    input wire clk,
+    input wire reset_n,
+    input wire signed [DATA_WIDTH-1:0] A [M-1:0][K-1:0], //Matrix A
+    input wire signed [DATA_WIDTH-1:0] B [K-1:0][N-1:0], //Matrix B
+    output wire signed [31:0] C [M-1:0][N-1:0];
+); 
+
+
+
+
+endmodule 
