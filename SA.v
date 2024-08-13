2fa04a1baf5ac8b0460ff7b5b09c1b07919a1251
Akhilendra Nair <mossbeachbrothers@gmail.com>
2024-08-13 14:45:18 -0700

    incorporated PE units into loop, TB file

--- SA.v
+++ SA.v
@@ -1,5 +1,5 @@
 //Systolic Array Module for M x N matrix
-module SystolicArray #(parameter M = 4, paramter K = 4, parameter N = 4, DATA_WIDTH=8)(
+module SystolicArray #(parameter M = 4, parameter K = 4, parameter N = 4, DATA_WIDTH=8)(
     input wire clk,
     input wire reset_n,
     input wire signed [DATA_WIDTH-1:0] A [M-1:0][K-1:0], //Matrix A (MxK)
@@ -21,10 +21,17 @@ generate;
         //for all columns of row
         for (j=0; j < N; j = j+1) begin : column
             if (j < K) begin
-                PE #() pe (
-
+                PE #(.DATA_WIDTH(DATA_WIDTH)) pe (
+                    .clk(clk),
+                    .reset_n(reset_n),
+                    .A_in((j == 0) ? A[i][j] : a[i][j-1]), //if first element, Ain taken from A, else from output of previous PE
+                    .B_in((i == 0) ? B[j][i] : b[j-1][i]), //if first element, Bin taken from B, else from output of previous PE
+                    .C_in((i == 0 || j == 0) ? 32'd0 : c[i-1][j-1]),
+                    .C_out(c[i][j]),
+                    .A_out(a[i][j]),
+                    .B_out(b[j][i])
                 );
-             end 0
+             end 
         end
     end
 endgenerate
