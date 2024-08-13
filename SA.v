9908a7e78562afec4a57ce0ba3b6a2093c524582
Akhilendra Nair <mossbeachbrothers@gmail.com>
2024-08-13 14:37:15 -0700

    inner loops

--- SA.v
+++ SA.v
@@ -14,6 +14,19 @@ wire signed [DATA_WIDTH-1:0] a [M-1:0][K-1:0]; //wire to propogate A across rows
 wire signed [DATA_WIDTH-1:0] b [K-1:0][N-1:0]; //wire to propogate B across columns
 wire signed [31:0] c [M-1:0][N-1:0]; //wires to hold results of MAC operation and pass to next PE, or output matrix 'C'
 
+genvar i,j; //define iteration variables
+generate;
+    //for all rows
+    for (i=0; i < M; i = i + 1) begin : row
+        //for all columns of row
+        for (j=0; j < N; j = j+1) begin : column
+            if (j < K) begin
+                PE #() pe (
 
+                );
+             end 0
+        end
+    end
+endgenerate
 
 endmodule 
